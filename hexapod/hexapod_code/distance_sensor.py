58b7d8164e94afa65a9dd6e7223f2534d9c9468c
jamieashton22 <jamie.ashton@student.manchester.ac.uk>
2026-03-21 16:32:09 +0000

    Fixed sensor class

--- hexapod/hexapod_code/distance_sensor.py
+++ hexapod/hexapod_code/distance_sensor.py
@@ -12,7 +12,7 @@ class distance_sensor:
 
     def __init__(self, trig_pin, echo_pin, timeout_us =30000):
         self.trig = Pin(trig_pin, Pin.OUT)
-        self.echo = Pin(echo_pin. Pin.IN)
+        self.echo = Pin(echo_pin, Pin.IN)
 
         self.timeout = timeout_us
 
@@ -32,13 +32,13 @@ class distance_sensor:
         start = time.ticks_us()
         while self.echo.value() == 0:
             if time.ticks_diff(time.ticks_us(), start) > self.timeout:
-                return -1
+                return None
         start_time = time.ticks_us()
 
         # Wait for echo end
         while self.echo.value() == 1:
             if time.ticks_diff(time.ticks_us(), start_time) > self.timeout:
-                return -1
+                return None
         end_time = time.ticks_us()
 
         # Calculate duration
@@ -47,5 +47,21 @@ class distance_sensor:
     def get_distance_cm(self):
 
         duration = self.send_pulse()
+
+        if duration is None:
+            return None
+        
         distance = (duration * 0.0343) / 2
         return distance
+    
+    # readings averaged over 5 samples
+    def get_distance_cm_avg(self, samples=5):
+            
+            readings = []
+
+            for _ in range(samples):
+                d = self.get_distance_cm()
+                if d is not None:
+                    readings.append(d)
+
+            return sum(readings) / len(readings) if readings else None
--- hexapod/hexapod_code/test_distance_sensor.py
+++ hexapod/hexapod_code/test_distance_sensor.py
@@ -1,11 +1,17 @@
 from distance_sensor import distance_sensor
 import time
 
-sensor = distance_sensor(trig_pin = 19, echo_pin = 18)
+sensor = distance_sensor(trig_pin=19, echo_pin=18)
 
-print("starting distance test")
+print("Starting distance test...")
 
 while True:
+
+    # # Bypass get_distance_cm and check raw pulse
+    # duration = sensor.send_pulse()
+    # print("Raw duration:", duration)
+
+
     dist = sensor.get_distance_cm()
 
     if dist == -1:
